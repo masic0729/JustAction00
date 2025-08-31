62574dbc27c92eb7e300f1a83ffdbccd4d0a82fc
masic0729 <pihlan0620@gmail.com>
2025-09-01 01:41:12 +0900

    Reapply "enemy attack complete"
    This reverts commit 68ee9f73057fba17ec8d12085b63ddc3bf8d187d.

--- ActionMake00/Assets/Scripts/Character/Enemy/Node/Boss/BossGolem.cs
+++ ActionMake00/Assets/Scripts/Character/Enemy/Node/Boss/BossGolem.cs
@@ -57,10 +57,12 @@ public class BossGolem : BossEnemyBT
         {
             Destroy(pattenEffect);
             pattenEffect = null;
-        }
-        else
-        {
 
+            float rotateValue = 25f;
+            GameObject instance = Instantiate(skillProjectiles[0], spawnProjectileTransform.position, spawnProjectileTransform.rotation);
+            instance.transform.Rotate(0, rotateValue, 0);
+            instance = Instantiate(skillProjectiles[0], spawnProjectileTransform.position, spawnProjectileTransform.rotation);
+            instance.transform.Rotate(0, -rotateValue, 0);
         }
 
 
