dac8fd543f0b021aa0bc94472241e061b93a1e38
Yuja <pihlan0620@gmail.com>
2025-10-30 21:00:17 +0900

    other option

--- ActionMake00/Assets/Scripts/Item/ItemBase.cs
+++ ActionMake00/Assets/Scripts/Item/ItemBase.cs
@@ -4,7 +4,7 @@ using System;
 public class ItemBase
 {
     public ItemData data;
-    //public ItemSlot slotData;                                        //해당 아이템이 위치한 슬롯 정보
+    //public ItemSlot slotData;                                      //해당 아이템이 위치한 슬롯 정보
     public SlotBase slotData;                                        //해당 아이템이 위치한 슬롯 정보
     public int addCount;                                             //해당 데이터는 테스트용으로 사용중임. 절대 이를 활용하지 말것
     public int currentCount;                                         //현재 아이템 슬롯에 쌓여있는 개수. (예시로 포션 같은 소비형 아이템이 여러개 쌓여있는 경우)
