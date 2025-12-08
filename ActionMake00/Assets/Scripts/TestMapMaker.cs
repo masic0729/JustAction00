dd4a7a01962b7254ae36d131d2caf27640c91886
masic0729 <pihlan0620@gmail.com>
2025-12-08 15:48:15 +0900

    타일맵 버그 발견 및 로직 분석 완료
    분석된 로직을 기반으로 한번 구현 후, 다시 한번 오류가 있는 지 확인할 것.

--- ActionMake00/Assets/Scripts/TestMapMaker.cs
+++ ActionMake00/Assets/Scripts/TestMapMaker.cs
@@ -2,13 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MAPTYPE
+{
+    START = 0,
+    STRAIGHT = 1,
+    LEFT,
+    RIGHT,
+    END
+}
+
+public class SpawnedMapData
+{
+    public List<int> spawnedMapX = new List<int>();
+    public List<int> spawnedMapZ = new List<int>();
+    public List<MAPTYPE> mapType = new List<MAPTYPE>();
+    public List<float> spawnedMapRotationY = new List<float>();
+}
+
 public class TestMapMaker : MonoBehaviour
 {
+    SpawnedMapData mapData;                     //추후 json에 저장할 데이터
+
     //맵은 테스트용이기에 1개 밖에 없지만,
     //좌/우/마지막 맵(보스 방)으로 구성되어 있다
     public GameObject Ground;
 
-    GameObject[] createdMap;
+    List <GameObject> createdMap = new List<GameObject>();
+
+    //맵의 형태에 따라서 회전값이 다를 것이다.
+    //이를 고려하여 맵 생성 후 데이터를 저장할 때 사용한다
+    float currentMapRotateY = 0f;
 
     public int mapMakeCount;
     public int currentX, currentZ;
@@ -17,7 +40,7 @@ public class TestMapMaker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        createdMap = new GameObject[mapMakeCount];
+        
 
         InitMap();
     }
@@ -31,26 +54,40 @@ public class TestMapMaker : MonoBehaviour
         }
     }
 
+
     public void InitMap()
     {
+        mapData = new SpawnedMapData();
+
+
+        
+
+
         List<(int, int)> mapIndex = new List<(int, int)>();
 
-        if (createdMap.Length > 0)
+        if (createdMap.Count > 0)
         {
-            for(int i = 0; i < createdMap.Length; i++)
+            while(createdMap.Count >0)
             {
-                Destroy(createdMap[i]);
+                Destroy(createdMap[0]);
+                createdMap.RemoveAt(0);
             }
         }
 
-        createdMap = new GameObject[mapMakeCount];
 
         bool[,] map;
 
         map = new bool[mapMakeCount, mapMakeCount];
+
         //처음 시작하는 맵 위치는 이곳이다.
         map[3, 3] = true;
         mapIndex.Add((3, 3));
+
+        mapData.spawnedMapX.Add(3);
+        mapData.spawnedMapZ.Add(3);
+        mapData.mapType.Add(MAPTYPE.START);
+        mapData.spawnedMapRotationY.Add(0f);
+
 
         //고정적으로 위로 올라가기 때문에 3,4로 이동한다.
         map[3, 4] = true;
@@ -67,37 +104,63 @@ public class TestMapMaker : MonoBehaviour
 
             if (currentX - 1 >= 0)
             {
-                if (map[currentX - 1, currentZ] == false) { canMakeMapList.Add(((currentX - 1), currentZ)); }
+                if (map[currentX - 1, currentZ] == false) 
+                {
+                    canMakeMapList.Add(((currentX - 1), currentZ));
+                }
             }
 
             if (currentX + 1 <= 6)
             {
-                if (map[currentX + 1, currentZ] == false) { canMakeMapList.Add(((currentX + 1), currentZ)); }
+                if (map[currentX + 1, currentZ] == false) 
+                {
+                    canMakeMapList.Add(((currentX + 1), currentZ)); 
+                }
             }
 
             if (currentZ - 1 >= 0)
             {
-                if (map[currentX, currentZ - 1] == false) { canMakeMapList.Add(((currentX), currentZ - 1)); }
+                if (map[currentX, currentZ - 1] == false) 
+                {
+                    canMakeMapList.Add(((currentX), currentZ - 1)); 
+                }
             }
 
             if (currentZ + 1 <= 6)
             {
-                if (map[currentX, currentZ + 1] == false) { canMakeMapList.Add(((currentX), currentZ + 1)); }
+                if (map[currentX, currentZ + 1] == false) 
+                {
+                    canMakeMapList.Add(((currentX), currentZ + 1)); 
+                }
             }
 
-            //제작할 수 있는 맵의 경우의 수면 데이터 삽입 및 진전
+            //제작할 수 있는 맵의 경우의 수라면 데이터 삽입 및 진전
             if (canMakeMapList.Count > 0)
             {
                 (int, int) targetMap = canMakeMapList[Random.Range(0, canMakeMapList.Count)];
+                //float resultRotateY = currentMapRotateY + targetMap.Item4;
+
+                SetMapType();
+                mapData.spawnedMapX.Add(currentX);
+                mapData.spawnedMapZ.Add(currentZ);
+
+                //currentMapRotateY = resultRotateY;
 
                 map[targetMap.Item1, targetMap.Item2] = true;
-                mapIndex.Add(targetMap);
+                mapIndex.Add((targetMap.Item1, targetMap.Item2));
+
                 currentMakeCount++;
+                
                 currentX = targetMap.Item1;
-                currentZ = targetMap.Item2;
+                currentZ = targetMap.Item2;    
             }
             else
             {
+                mapData.spawnedMapX.RemoveAt(mapData.spawnedMapX.Count);
+                mapData.spawnedMapZ.RemoveAt(mapData.spawnedMapZ.Count);
+                mapData.mapType.RemoveAt(mapData.mapType.Count);
+                mapData.spawnedMapRotationY.RemoveAt(mapData.spawnedMapRotationY.Count);
+
                 map[currentX, currentZ] = false;
 
                 mapIndex.RemoveAt(mapIndex.Count - 1);
@@ -112,12 +175,36 @@ public class TestMapMaker : MonoBehaviour
 
         }
 
+        mapData.spawnedMapX.Add(currentX);
+        mapData.spawnedMapZ.Add(currentZ);
+        mapData.mapType.Add(MAPTYPE.END);
+        mapData.spawnedMapRotationY.Add(currentMapRotateY);
+
         //디버깅용
         for (int i = 0; i < mapIndex.Count; i++)
         {
-            Debug.Log(mapIndex[i]);
             Vector3 spawnPos = new Vector3(mapIndex[i].Item1 * 3, 0, mapIndex[i].Item2 * 3);
-            createdMap[i] = Instantiate(Ground, spawnPos, transform.rotation);
+            Debug.Log(mapData.spawnedMapX[i] + " " + mapData.spawnedMapZ[i] + " " + mapData.mapType[i] + " " + mapData.spawnedMapRotationY[i]);
+
+            createdMap.Add(Instantiate(Ground, spawnPos, transform.rotation));
+
+            //3D 타일 회전
+            createdMap[createdMap.Count - 1].transform.Rotate(0, mapData.spawnedMapRotationY[i], 0);
         }
+
+        
+
     }
+
+    void SetMapType()
+    {
+        
+    }
+
+    void SetMapRotate()
+    {
+
+    }
+
+
 }
