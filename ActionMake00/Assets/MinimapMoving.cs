7258b68678d579ee416b7ff5f6dcac4d8417894c
masic0729 <pihlan0620@gmail.com>
2026-04-13 14:10:19 +0900

    미니맵 및 가이드 구현

---
+++ ActionMake00/Assets/MinimapMoving.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapMoving : MonoBehaviour
+{
+    [SerializeField]GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveByPlayer();
+    }
+
+    void MoveByPlayer()
+    {
+        transform.position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+    }
+}
